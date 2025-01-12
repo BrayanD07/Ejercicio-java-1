79c6d623b001169e186d83e716a5644b8d0b586d
Brayan Diaz <52518417+BrayanD07@users.noreply.github.com>
2025-01-12 12:09:30 -0600

    Add files via upload

---
+++ IntroduccionJava/src/HolaMundo.java
@@ -0,0 +1,64 @@
+//import java.security.SecureRandom;
+//import java.sql.SQLOutput;
+//import java.util.Random;
+//import java.util.Scanner;
+public class HolaMundo 
+{
+    public static void main(String[] args) 
+    {
+
+     //  String nameUser = "Brayan Everardo Granados Diaz";
+     //  String nameEmpresa = "Global mentoring";
+     //  String dominio = ".com.mx";
+     //
+     //  String nameUserNormalizado = nameUser.toLowerCase().replace(" ",".");
+     //  String dominioNormalizado = "@" + nameEmpresa.toLowerCase().replace(" ","") + dominio;
+     //
+     //  System.out.println(nameUserNormalizado + dominioNormalizado);
+
+
+     //  var console = new Scanner(System.in);
+     //
+     //  System.out.println("*Recetas de Cocina*");
+     //  System.out.print("Ingresa el nombre: ");
+     //  var nombreReceta = console.nextLine();
+     //
+     //  System.out.print("Ingresa los ingredientes: ");
+     //  var ingredientesReceta = console.nextLine();
+     //
+     //  System.out.print("Ingresa el tiempo de preparacion: ");
+     //  var tiempoPreparacionReceta = Integer.parseInt(console.nextLine());
+     //
+     //  System.out.print("Ingresa la dificultad: ");
+     //  var dificultadReceta = console.nextLine();
+     //
+     //  System.out.println("\n--- Receta de cocina ---");
+     //  System.out.println("Nombre receta: " + nombreReceta);
+     //  System.out.println("Ingredientes: " + ingredientesReceta);
+     //  System.out.println("Tiempo de preparacion: " + tiempoPreparacionReceta + " minutos");
+     //  System.out.println("Dificultad: " + dificultadReceta);
+
+     //  var rand = new Random();
+
+     //  System.out.print("Ingrese Nombre: ");
+     //  var nombreEmpreado = console.nextLine();
+
+     //  System.out.print("Ingrese Apellido: ");
+     //  var apellidoEmpreado = console.nextLine();
+
+     //  System.out.print("Ingrese año de nacimiento: ");
+     //  var nacimientoEmpreado = console.nextLine();
+
+     //  var numero = rand.nextInt(9998) + 1;
+
+     //  var idEmpleado = nombreEmpreado.substring(0,2).toUpperCase() + apellidoEmpreado.substring(0,2).toUpperCase() + nacimientoEmpreado.substring(2);
+
+     //  System.out.print("Tu nuevo ID es: ");
+     //  System.out.printf(idEmpleado + "%04d", numero);
+
+      int a=2; 
+      System.out.print("a = ");
+      System.out.println(+++a);
+    }
+}
+
